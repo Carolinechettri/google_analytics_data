e682b1d905064834404788fe462f419e53ba2e33
Carolinechettri <carolinechettri96@gmail.com>
2025-10-25 15:02:50 +0200

    Volume of users + categories / country
    Looking at the volume of users along with the categories per user

--- google_analytics.sql
+++ google_analytics.sql
@@ -8,3 +8,13 @@ visitId
 , geoNetwork.country
 FROM `my-projects-475112.google_analytics_test.ga_sessions_raw`
 , UNNEST(hits)
+---add volume of users/country
+SELECT
+  COUNT(visitId) AS nb_visitors,
+  geoNetwork.country,
+  product.v2ProductCategory
+FROM `my-projects-475112.google_analytics_test.ga_sessions_raw`
+, UNNEST(hits) AS hit
+, UNNEST(hit.product) AS product
+GROUP BY geoNetwork.country, product.v2ProductCategory
+ORDER BY nb_visitors DESC
