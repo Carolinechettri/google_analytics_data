eb439aaa9120ce4de6c8c106d8ed6e5c3c3f8c4a
Carolinechettri <carolinechettri96@gmail.com>
2025-11-14 08:44:39 +0100

    cleaning up unused files

--- page_views_explo.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-visitId
-, PARSE_DATE('%Y%m%d', `date`) AS `date`
-, ROUND(SAFE_DIVIDE(`time`, 60000),2) AS mins_spent
-, CONCAT(CAST(hour AS STRING), ":", LPAD(CAST(minute AS STRING),2,'0')) AS `time`
-, page.pagePath
-, page.pageTitle
-FROM `my-projects-475112.google_analytics_test.ga_sessions_raw`
-, UNNEST(hits) AS hit
